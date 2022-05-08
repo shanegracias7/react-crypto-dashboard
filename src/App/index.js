8cf4141d56f3bc4b2a7a3a101df0bcc4fd6be10d
shanegracias <shanegracias7@gmail.com>
2022-05-08 09:54:29 +0530

    setting up / expeimenting styled componet

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,28 @@
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled,{css} from 'styled-components';
+
+const MyButton = styled.button`
+  color: green;
+  ${props => props.primary && css`
+    background: green;
+    color: black;
+  `}
+  `
+
+  const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
-    <div>hello</div>
+    <div>
+    <WelcomeMessage/>
+    <MyButton primary={false}>hello</MyButton>
+    <TomatoButton primary={true}>hello</TomatoButton>
+    </div>
+    
   );
 }
 
