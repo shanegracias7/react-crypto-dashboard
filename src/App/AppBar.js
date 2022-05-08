2024a2b74c1f3bc70618388966c5531167ce0f2a
shanegracias <shanegracias7@gmail.com>
2022-05-08 16:12:51 +0530

    styled Appbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import styled,{css} from 'styled-components';
 
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 const Bar = styled.div`
     display:grid;
     grid-template-columns: 180px auto 100px 100px;
 `
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    ${props=>props.active&&css`
+        color:blue;
+        margin-bottom: 40px;
+        text-shadow: 0px 0px 50px #ffffff;
+    `}
+`
+function ControlButton({name , active}){
+    return (
+        <ControlButtonElem active = {active}>
+            {name}
+        </ControlButtonElem>
+    )
+}
 
 export default function AppBar() {
   return (
     <Bar>
-        <div>cryptodashboard</div>
-        <div></div>
-        <div>dashboard</div>
-        <div>settings</div>
+        <Logo>cryptodashboard</Logo>
+        <ControlButton/>
+        <ControlButton name={"dashboard"} active/>
+        <ControlButton name={"settings"}/>
     </Bar>
   )
 }
