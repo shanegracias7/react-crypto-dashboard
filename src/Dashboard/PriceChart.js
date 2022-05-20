570176cb09be75a2b08b64479c1c06701c1d3792
shanegracias <shanegracias7@gmail.com>
2022-05-20 09:53:50 +0530

    styled the chart

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,6 +3,8 @@ import ReactHighcharts from "react-highcharts";
 import highchartsConfig from "./HighchartsConfig";
 import { Tile } from "../Shared/Tile";
 import { AppContext } from "../App/AppProvider";
+import HighchartsTheme from './HighchartsTheme';
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function(){
     return(
